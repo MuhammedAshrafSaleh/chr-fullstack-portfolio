9a76b7935e101e49f9fa813a992d91e4c726dc07
MuhammedAshrafSaleh <muhammedashrafsaleh@gmail.com>
2026-03-06 21:23:48 +0200

    Fix Repeated Code in App.css

--- resources/views/frontend/layout/side_actions.blade.php
+++ resources/views/frontend/layout/side_actions.blade.php
@@ -21,15 +21,6 @@
             style="width: 20px; height: 20px; object-fit: contain;">
     </a>
 
-    {{-- <a href="https://wa.me/yournumber" class="side-actions__btn side-actions__btn--whatsapp" target="_blank"
-        rel="noopener">
-        <i class="fab fa-whatsapp"></i>
-    </a>
-
-    <a href="https://goo.gl/maps/yourlink" class="side-actions__btn" target="_blank" rel="noopener">
-        <i class="fas fa-map-marker-alt"></i>
-    </a> --}}
-
     <div class="side-actions__social-group">
         <button class="side-actions__btn side-actions__trigger">
             <i class="fas fa-share-alt"></i>
@@ -44,10 +35,5 @@
                 </a>
             @endforeach
         </div>
-        {{-- <div class="side-actions__sub-menu">
-            <a href="#" class="side-actions__btn side-actions__btn--sub"><i class="fab fa-facebook-f"></i></a>
-            <a href="#" class="side-actions__btn side-actions__btn--sub"><i class="fab fa-instagram"></i></a>
-            <a href="#" class="side-actions__btn side-actions__btn--sub"><i class="fab fa-linkedin-in"></i></a>
-        </div> --}}
     </div>
 </aside>
